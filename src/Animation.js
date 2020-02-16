cdd1b099e158f1b084c0cde03e6723052a6205b9
manniepack <intrnt.dolphin@gmail.com>
2020-02-16 00:56:31 -0800

    Massive refactoring; implement mouse-eye tracking
    While everything fits together fairly well,
    I'm somewhat unhappy with the overall
    structure of the Animation component --
    it's way too humongous.
    
    I'm uncertain about continuing to use React
    but I'm not interested in writing another
    reactive UI library.
    ---
    
    The eye-tracking is fairly cool. Glitchy if
    the mouse is too close. A fix would be to
    simply stop tracking when the mouse is on
    the character's face. I'll consider rewo-
    rking that later.

--- src/Animation.js
+++ src/Animation.js
@@ -13,8 +13,16 @@ class Animation extends React.Component {
 
     this.getRendererSize = this.getRendererSize.bind(this);
     this.updateAnimationSize = this.updateAnimationSize.bind(this);
+
+    this.buildAnimation = this.buildAnimation.bind(this);
     this.loadAnimationAssets = this.loadAnimationAssets.bind(this);
     this.generateSprites = this.generateSprites.bind(this);
+    this.buildInteractions = this.buildInteractions.bind(this);
+    this.showAnimation = this.showAnimation.bind(this);
+
+    this.state = {
+      isLoaded: false,
+    };
 
     /**
      * CANVAS_SIZE are the dimensions the original
@@ -70,39 +78,63 @@ class Animation extends React.Component {
         interactive: true,
       }),
       ticker: new PIXI.Ticker(),
-      sprites: {},
+      sprites: {
+        frame: null,
+        char: null,
+        char_eye: {
+          left: null,
+          right: null,
+          sclera: null,
+        },
+        waves: {
+          0: null,
+          1: null,
+          2: null,
+          3: null,
+        },
+        sky: null,
+      },
     };
   }
 
   componentDidMount() {
-    const { renderer, stage, ticker } = this.PIXI_APP;
-
     /**
-     * TODO: Redo asset loading state
-     *
-     * Considering intitializing
+     * Does this also chain load:
+     *   - sprites?
+     *   - interactions?
+     *   - EVENTS?!?
+     * Yes.
+     * 
+     * Should it?
+     * 
+     * Probably not.
+     * 
+     * WARNING: Highly impure function,
+     * but that's the beauty of custom-
+     * ized software. Everything is
+     * ~~arbitrarily tied to each
+     * other~~ specifically tailored.
      */
-    this.loadAnimationAssets();
-
-    ticker.add(
-      () => { renderer.render(stage); },
-      PIXI.UPDATE_PRIORITY.HIGH
-    );
-
-    ticker.start();
-    this.ANIMATION_ROOT.appendChild(renderer.view);
+    // await this.buildAnimation(); (can await if needed)
+    this.buildAnimation();
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps, prevState) {
     const prevParentSize = prevProps.parentSize;
     const { parentSize } = this.props;
 
     if (
-      prevParentSize.width === parentSize.width &&
-      prevParentSize.height === parentSize.height
-    ) return;
+      prevParentSize.width !== parentSize.width ||
+      prevParentSize.height !== parentSize.height
+    ) {
+      this.updateAnimationSize();
+    }
 
-    this.updateAnimationSize();
+    const prevLoaded = prevState.isLoaded;
+    const { isLoaded } = this.state;
+    if (isLoaded && !prevLoaded) {
+      this.ANIMATION_ROOT.appendChild(this.PIXI_APP.renderer.view);
+    }
   }
 
   componentWillUnmount() {
@@ -111,6 +143,8 @@ class Animation extends React.Component {
   }
 
   render() {
+    const { isLoaded } =  this.state;
+
     const { width, height } = this.props.parentSize;
     const parentStyle = {
       width,
@@ -118,9 +152,335 @@ class Animation extends React.Component {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
+      transition: 'background-color 0.3s ease-out',
     };
 
+    if (!isLoaded) {
+
+      const loaderParentStyle = {
+        ...parentStyle,
+        backgroundColor: 'black',
+        color: 'white',
+      }
+
+      const loaderStyle = {
+        textAlign: 'center',
+      };
+
+      return (
+        <div style={loaderParentStyle}>
+          <div style={loaderStyle}>
+            ...pitching, and moaning...
+          </div>
+        </div>
+      );
+    }
+
     return <div style={parentStyle} ref={elem => this.ANIMATION_ROOT = elem} />;
+  }
+
+  async buildAnimation() {
+    await this.loadAnimationAssets();
+    this.generateSprites();
+    this.buildInteractions();
+    this.showAnimation();
+  }
+
+  async loadAnimationAssets() {
+    return new Promise(resolve => {
+      const { ASSET } = this;
+
+      PIXI.Loader.shared
+        .add(ASSET('frame.png'))
+        .add(ASSET('sky.png'))
+        .add(ASSET('ss_char.json'))
+        .add(ASSET('ss_sea.json'))
+        .load(resolve);
+    });
+  }
+
+  generateSprites() {
+    const { ASSET } = this;
+    const loader = PIXI.Loader.shared;
+
+    const frame = new PIXI.Sprite(loader.resources[ASSET('frame.png')].texture);
+    const sky = new PIXI.Sprite(loader.resources[ASSET('sky.png')].texture);
+
+    /**
+     * (char) David Eves himself, oh man.
+     * (char._STATES.*)
+     * 
+     * I'm storing interactive information in
+     * _DEFAULT, _HOVER, and _CLICK on the
+     * sprite respectively.
+     * 
+     * (so texture & position)
+     */
+    const ss_char = loader.resources[ASSET('ss_char.json')].spritesheet.textures;
+    const char = new PIXI.Sprite(ss_char['char.png']);
+    const char_eye_sclera = new PIXI.Sprite(PIXI.Texture.WHITE);
+    const char_eye_left = new PIXI.Sprite(ss_char['eye_left.png']);
+    const char_eye_right = new PIXI.Sprite(ss_char['eye_right.png']);
+    (() => {
+      char.anchor.set(0.5);
+      char.position.set(1611, 1385.50);
+      char._STATES = {
+        _DEFAULT: {
+          texture: ss_char['char.png'],
+          position: [1611, 1385.50],
+        },
+        _HOVER: {
+          texture: ss_char['char_hover.png'],
+          position: new PIXI.Point(1611, 1381.90),
+        },
+        _CLICK: {
+          texture: ss_char['char_click.png'],
+          position: new PIXI.Point(0, 0), // TODO
+        },
+      };
+  
+      /**
+       * The left and right eye store a _CENTER
+       * value that acts as the origin for mouse-
+       * tracking.
+       */
+      char_eye_left._CENTER = { x: 1545, y: 1024.50 };
+      char_eye_right._CENTER = { x: 1679.50, y: 1023.50 };
+  
+      char_eye_left.anchor.set(0.5);
+      char_eye_right.anchor.set(0.5);
+      
+      char_eye_left.position.set(char_eye_left._CENTER.x, char_eye_left._CENTER.y);
+      char_eye_right.position.set(char_eye_right._CENTER.x, char_eye_right._CENTER.y);
+
+      /**
+       * (char_eye_*) Sprites specific to
+       * character eye interaction (eyes
+       * mouse follow).
+       * 
+       * _sclera is a flat white rendered
+       * below the irises, allowing the
+       * illusion of a moving eyeball that
+       * tracks user mouse/touch events.
+       */
+      char_eye_sclera.width = 221;
+      char_eye_sclera.height = 79;
+      char_eye_sclera.anchor.set(0.5); 
+      char_eye_sclera.position.set(1610.50, 1023.50);
+    })();
+
+    /**
+     * wave_*
+     * 
+     * Watch 'em go.
+     */
+    const ss_sea = loader.resources[ASSET('ss_sea.json')].spritesheet.textures;
+    const wave_0 = new PIXI.Sprite(ss_sea['wave_1.png']);
+    const wave_1 = new PIXI.Sprite(ss_sea['wave_2.png']);
+    const wave_2 = new PIXI.Sprite(ss_sea['wave_3.png']);
+    const wave_3 = new PIXI.Sprite(ss_sea['wave_4.png']);
+    (() => {
+      wave_0.anchor.set(0.5);
+      wave_0.position.set(2444.50, 1386.50);
+  
+      wave_1.anchor.set(0.5);
+      wave_1.position.set(823.50, 1253.50);
+  
+      wave_2.anchor.set(0.5);
+      wave_2.position.set(1686.50, 1545.50);
+  
+      wave_3.anchor.set(0.5);
+      wave_3.position.set(954, 1604);
+    })();
+
+    this.PIXI_APP.sprites = {
+      frame,
+      char,
+      char_eye: {
+        left: char_eye_left,
+        right: char_eye_right,
+        sclera: char_eye_sclera,
+      },
+      waves: {
+        0: wave_0,
+        1: wave_1,
+        2: wave_2,
+        3: wave_3,
+      },
+      sky,
+    };
+  }
+
+  buildInteractions() {
+    const { renderer, stage, sprites, ticker } = this.PIXI_APP;
+
+    /**
+     * @param {x, y} pos 
+     */
+    const screenPosToCanvas = pos => {
+      const { stage } = this.PIXI_APP;
+      const { CANVAS_SIZE } = this;
+
+      const xFactor = CANVAS_SIZE.width / stage.width;
+      const yFactor = CANVAS_SIZE.height / stage.height;
+
+      /**
+       * What are matrices lmao? 🤔
+       */
+      return {
+        x: pos.x * xFactor,
+        y: pos.y * yFactor,
+      };
+    }
+
+    // TODO:
+    // check for interactions
+    // * char (hover|click)
+
+    const trackTargetWithEye = target => {
+      for (let eye in this.PIXI_APP.sprites.char_eye) {
+        if (eye === 'sclera') continue;
+
+        eye = this.PIXI_APP.sprites.char_eye[eye];
+
+        const targetVector = {
+          x: target.x - eye.x,
+          y: target.y - eye.y,
+        };
+        const distance = Math.sqrt(Math.pow(targetVector.x, 2) + Math.pow(targetVector.y, 2));
+
+        targetVector.x = targetVector.x / distance;
+        targetVector.y = targetVector.y > 1 ? targetVector.y * 2.32 / distance : targetVector.y / distance;
+
+        eye.position.set(eye._CENTER.x + (13 * targetVector.x), eye._CENTER.y + (5 * targetVector.y));
+      }
+    }
+
+    stage.interactive = true;
+    stage.on('mousemove', event => {
+      trackTargetWithEye(screenPosToCanvas(event.data.global));
+    });
+
+    /**
+     * Wave Interactions
+     * 
+     * Each wave has an _ANIMATION property
+     * that stores and configures its own
+     * animated properties.
+     * 
+     * _ANIMATION.func is automatically added
+     * to the main ticker (for each wave).
+     */
+    const waves = sprites.waves;
+    (() => {
+      waves[0]._ANIMATION = {
+        value: 0,
+        changeInValue: 0.0035,
+        movement: 0.23,
+        rotation: 0.0125,
+        func: time => {
+          const wave = waves[0];
+          let { value } = wave._ANIMATION;
+          if (value > PIXI.PI_2) value = 0;
+          
+          const { changeInValue, movement, rotation } = wave._ANIMATION;
+          wave.position.y += movement * Math.sin(value);
+          wave.rotation = rotation * Math.sin(value);
+  
+          wave._ANIMATION.value = value + changeInValue * time
+        },
+      };
+
+      waves[1]._ANIMATION = {
+        value: 0,
+        changeInValue: 0.0037,
+        movement: 0.1,
+        rotation: 0.0125,
+        func: time => {
+          const wave = waves[1];
+          let { value } = wave._ANIMATION;
+          if (value > PIXI.PI_2) value = 0;
+          
+          const { changeInValue, movement, rotation } = wave._ANIMATION;
+          wave.position.y += movement * Math.cos(value);
+          wave.rotation = rotation * Math.sin(value);
+  
+          wave._ANIMATION.value = value + changeInValue * time
+        },
+      };
+
+      waves[2]._ANIMATION = {
+        value: 0,
+        changeInValue: 0.0125,
+        movement: 0.3,
+        rotation: 0.0225,
+        func: time => {
+          const wave = waves[2];
+          let { value } = wave._ANIMATION;
+          if (value > PIXI.PI_2) value = 0;
+          
+          const { changeInValue, movement, rotation } = wave._ANIMATION;
+          wave.position.y += movement * Math.sin(value);
+          wave.rotation = rotation * Math.sin(value);
+  
+          wave._ANIMATION.value = value + changeInValue * time
+        },
+      };
+
+      waves[3]._ANIMATION = {
+        value: 0,
+        changeInValue: 0.0175,
+        movement: 0.2,
+        rotation: 0.0300,
+        func: time => {
+          const wave = waves[3];
+          let { value } = wave._ANIMATION;
+          if (value > PIXI.PI_2) value = 0;
+          
+          const { changeInValue, movement, rotation } = wave._ANIMATION;
+          wave.position.y += movement * Math.sin(value);
+          wave.rotation = rotation * Math.sin(value);
+  
+          wave._ANIMATION.value = value + changeInValue * time
+        },
+      };
+
+      for (let wave in waves) {
+        wave = waves[wave];
+        ticker.add(wave._ANIMATION.func);
+      }
+    })();
+
+    /**
+     * Root update -- this runs everything.
+     */
+    ticker.add(
+      () => { renderer.render(stage); },
+      PIXI.UPDATE_PRIORITY.HIGH
+    );
+  }
+
+  showAnimation() {
+    const { stage, ticker, sprites } = this.PIXI_APP;
+
+    stage.addChild(sprites.sky);
+    for (let wave in sprites.waves) {
+      wave = sprites.waves[wave];
+      stage.addChild(wave);
+    }
+    stage.addChild(sprites.char_eye.sclera);
+    stage.addChild(sprites.char_eye.left);
+    stage.addChild(sprites.char_eye.right);
+    stage.addChild(sprites.char);
+    stage.addChild(sprites.frame);
+
+    this.updateAnimationSize();
+    ticker.start();
+
+    this.setState({
+      ...this.state,
+      isLoaded: true,
+    });
   }
 
   getRendererSize(container=this.props.parentSize, target=this.CANVAS_SIZE) {
@@ -138,150 +498,6 @@ class Animation extends React.Component {
     stage.width = width;
     stage.height = height;
     renderer.resize(width, height);
-  }
-
-  loadAnimationAssets() {
-    const { ASSET } = this;
-
-    PIXI.Loader.shared
-      .add(ASSET('frame.png'))
-      .add(ASSET('sky.png'))
-      .add(ASSET('ss_char.json'))
-      .add(ASSET('ss_sea.json'))
-      .load(this.generateSprites);
-  }
-
-  generateSprites() {
-    const { ASSET } = this;
-    const { stage, ticker } = this.PIXI_APP;
-    const loader = PIXI.Loader.shared;
-
-    const frame = new PIXI.Sprite(loader.resources[ASSET('frame.png')].texture);
-    const sky = new PIXI.Sprite(loader.resources[ASSET('sky.png')].texture);
-
-    const ss_sea = loader.resources[ASSET('ss_sea.json')].spritesheet.textures;
-    const wave_1 = new PIXI.Sprite(ss_sea['wave_1.png']);
-    const wave_2 = new PIXI.Sprite(ss_sea['wave_2.png']);
-    const wave_3 = new PIXI.Sprite(ss_sea['wave_3.png']);
-    const wave_4 = new PIXI.Sprite(ss_sea['wave_4.png']);
-
-    const ss_char = loader.resources[ASSET('ss_char.json')].spritesheet.textures;
-    const char_eye_sclera = new PIXI.Sprite(PIXI.Texture.WHITE);
-    const char_eye_left = new PIXI.Sprite(ss_char['eye_left.png']);
-    const char_eye_right = new PIXI.Sprite(ss_char['eye_right.png']);
-    const char = new PIXI.Sprite(ss_char['char.png']);
-
-    stage.addChild(sky);
-    stage.addChild(wave_1);
-    stage.addChild(wave_2);
-    stage.addChild(wave_3);
-    stage.addChild(wave_4);
-    stage.addChild(char_eye_sclera);
-    stage.addChild(char_eye_left);
-    stage.addChild(char_eye_right);
-    stage.addChild(char);
-    stage.addChild(frame);
-
-    wave_1.anchor.set(0.5);
-    wave_1.position.set(2444.50, 1386.50);
-
-    wave_2.anchor.set(0.5);
-    wave_2.position.set(823.50, 1253.50);
-
-    wave_3.anchor.set(0.5);
-    wave_3.position.set(1686.50, 1545.50);
-
-    wave_4.anchor.set(0.5);
-    wave_4.position.set(954, 1604);
-
-    char_eye_sclera.width = 221;
-    char_eye_sclera.height = 79;
-    char_eye_sclera.anchor.set(0.5); 
-    char_eye_sclera.position.set(1610.50, 1023.50);
-
-    char_eye_left.CENTER = { x: 1545, y: 1024.50 };
-    char_eye_left.anchor.set(0.5);
-    char_eye_left.position.set(char_eye_left.CENTER.x, char_eye_left.CENTER.y);
-
-    char_eye_right.CENTER = { x: 1679.50, y: 1023.50 };
-    char_eye_right.anchor.set(0.5);
-    char_eye_right.position.set(char_eye_right.CENTER.x, char_eye_right.CENTER.y);
-
-    /**
-     * char
-     */
-    char.anchor.set(0.5);
-    char.position.set(1611, 1385.50);
-    char.cursor = 'pointer';
-    char.interactive = true;
-
-    const char_hover = e => {
-      char.texture = ss_char['char_hover.png'];
-      char.anchor.set(0.5);
-      char.position.set(1611, 1381.90);
-    };
-    char.on('mouseover', char_hover);
-
-    const char_default = e => {
-      char.texture = ss_char['char.png'];
-      char.anchor.set(0.5);
-      char.position.set(1611, 1385.50);
-    }
-    char.on('mouseout', char_default);
-
-    /**
-     * wave_*
-     */
-    wave_1.CHANGE = 0;
-    wave_2.CHANGE = 0;
-    wave_3.CHANGE = 0;
-    wave_4.CHANGE = 0;
-    ticker.add(
-      (time) => {
-        if (wave_1.CHANGE > PIXI.PI_2) wave_1.CHANGE = 0;
-        if (wave_2.CHANGE > PIXI.PI_2) wave_2.CHANGE = 0;
-        if (wave_3.CHANGE > PIXI.PI_2) wave_3.CHANGE = 0;
-        if (wave_4.CHANGE > PIXI.PI_2) wave_4.CHANGE = 0;
-
-        wave_1.position.y += 0.23 * Math.sin(wave_1.CHANGE);
-        wave_1.rotation = 0.0125 * Math.sin(wave_1.CHANGE);
-        wave_2.position.y += 0.1 * Math.cos(wave_2.CHANGE);
-        wave_2.rotation = 0.0125 * Math.sin(wave_2.CHANGE);
-        wave_3.position.y += 0.3 * Math.sin(wave_3.CHANGE);
-        wave_3.rotation = 0.0225 * Math.sin(wave_3.CHANGE);
-        wave_4.position.y += 0.2 * Math.sin(wave_4.CHANGE);
-        wave_4.rotation = 0.0300 * Math.sin(wave_4.CHANGE);
-
-        wave_1.CHANGE += 0.0035 * time;
-        wave_2.CHANGE += 0.0037 * time;
-        wave_3.CHANGE += 0.0125 * time;
-        wave_4.CHANGE += 0.0175 * time;
-      },
-      PIXI.UPDATE_PRIORITY.INTERACTION
-    );
-
-    this.PIXI_APP._sprites = {
-      frame,
-      char: {
-        char,
-        char_default,
-        charHover: char_hover,
-        eye: {
-          sclera: char_eye_sclera,
-          left: char_eye_left,
-          right: char_eye_right,
-        },
-      },
-      waves: {
-        1: wave_1,
-        2: wave_2,
-        3: wave_3,
-        4: wave_4,
-      },
-      sky,
-    };
-
-    this.updateAnimationSize();
   }
 }
 
