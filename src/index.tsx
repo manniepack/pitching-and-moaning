2441e3a3ebca9dbc535449836a79eee37b7147dd
manniepack <intrnt.dolphin@gmail.com>
2020-03-17 23:41:45 -0700

    Fix window resize issue (remove action from autorun)

--- src/index.tsx
+++ src/index.tsx
@@ -87,7 +87,7 @@ const pixiApp: IGraphics = {
 };
 
 //   -> utility function to update pixiApp dimensions
-const updatePixiSize = MobX.action((app: IGraphics) => {
+const updatePixiSize = (app: IGraphics) => {
 
   //   -> use adaptive-scale to scale viewport to fit design
   const canvasSize = AS.getScaledRect({
@@ -101,7 +101,7 @@ const updatePixiSize = MobX.action((app: IGraphics) => {
   app.stage.position.set(canvasSize.x,canvasSize.y);
   app.stage.width = canvasSize.width;
   app.stage.height = canvasSize.height;
-});
+};
 
 //   -> stage stage every tick
 PIXI.Ticker.system.add(() => {
@@ -191,47 +191,61 @@ PIXI.Loader.shared
     cosmicBackgroundRadiation.width = CANVAS_SIZE[0];
     cosmicBackgroundRadiation.height = CANVAS_SIZE[1];
 
+
     sky.texture = idx(resources, _ => _.sky.texture) || PIXI.Texture.WHITE;
     sky.anchor.set(0.5);
     sky.position.set(1599.50, 1148.50);
+
     lightning[0].texture = idx(resources, _ => _.lightning.textures['lightning1.png']) || PIXI.Texture.WHITE;
     lightning[0].anchor.set(0.5)
     lightning[0].position.set(sky.position.x - 1124.50, sky.position.y - 1276.50);
+
     lightning[1].texture = idx(resources, _ => _.lightning.textures['lightning2.png']) || PIXI.Texture.WHITE;
     lightning[1].anchor.set(0.5)
     lightning[1].position.set(sky.position.x - 1670.00, sky.position.y - 1249.00);
+
     lightning[2].texture = idx(resources, _ => _.lightning.textures['lightning3.png']) || PIXI.Texture.WHITE;
     lightning[2].anchor.set(0.5)
     lightning[2].position.set(sky.position.x - 2195.00, sky.position.y - 1229.50);
+
     sky.addChild(...lightning);
+
 
     waves[0].texture = idx(resources, _ => _.waves.textures['wave1.png']) || PIXI.Texture.WHITE;
     waves[0].anchor.set(0.5);
     waves[0].position.set(954.00, 1604.00);
+
     waves[1].texture = idx(resources, _ => _.waves.textures['wave2.png']) || PIXI.Texture.WHITE;
     waves[1].anchor.set(0.5);
     waves[1].position.set(1686.50, 1545.50);
+
     waves[2].texture = idx(resources, _ => _.waves.textures['wave3.png']) || PIXI.Texture.WHITE;
     waves[2].anchor.set(0.5);
     waves[2].position.set(823.50, 1253.50);
+
     waves[3].texture = idx(resources, _ => _.waves.textures['wave4.png']) || PIXI.Texture.WHITE;
     waves[3].anchor.set(0.5);
     waves[3].position.set(2444.50, 1386.50);
+
 
     char_EyeSclera.texture = PIXI.Texture.WHITE;
     char_EyeSclera.width = 221;
     char_EyeSclera.height = 79;
     char_EyeSclera.anchor.set(0.5);
     char_EyeSclera.position.set(1610.50, 1023.50);
+
     char_EyeLeft.texture = idx(resources, _ => _.char.textures['eye_left.png']) || PIXI.Texture.WHITE;
     char_EyeLeft.anchor.set(0.5);
     char_EyeLeft.position.set(1545.00, 1024.50);
+
     char_EyeRight.texture = idx(resources, _ => _.char.textures['eye_right.png']) || PIXI.Texture.WHITE;
     char_EyeRight.anchor.set(0.5);
     char_EyeRight.position.set(1679.50, 1023.50);
+
     char.texture = idx(resources, _ => _.char.textures['char.png']) || PIXI.Texture.WHITE;
     char.anchor.set(0.5);
     char.position.set(1611.00, 1380.50);
+
 
     frame.texture = idx(resources, _ => _.frame.texture) || PIXI.Texture.WHITE;
     frame.anchor.set(0.5);
@@ -254,7 +268,6 @@ PIXI.Loader.shared
 
     //   -> trigger pixi resize to ensure fit
     updatePixiSize(pixiApp);
-
     //   -> disable isLoading state, which triggers animation load
     isLoading.set(false);
   }));
